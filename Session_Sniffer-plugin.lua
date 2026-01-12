1effa2d1d0366df9d4a218ba482ecabcc1ff3b03
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2026-01-12 07:47:47 +0100

    Refactor dec_to_ipv4 for improved readability
    Reformatted the dec_to_ipv4 function to use multi-line formatting, enhancing code readability and maintainability.

--- Session_Sniffer-plugin.lua
+++ Session_Sniffer-plugin.lua
@@ -64,7 +64,14 @@ local function handle_script_exit(params)
 end
 
 local function dec_to_ipv4(ip)
-    return string.format("%i.%i.%i.%i", ip >> 24 & 255, ip >> 16 & 255, ip >> 8 & 255, ip & 255)
+    return string.format(
+        "%i.%i.%i.%i",
+        ip >> 24 & 255,
+        ip >> 16 & 255,
+        ip >> 8 & 255,
+        ip & 255
+    )
+end
 end
 
 local function extract_valid_player_data(player)
